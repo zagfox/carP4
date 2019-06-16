ef75362b32727d669b01e011c7077a54274f484d
zagfox <feng.zhu1991@gmail.com>
2019-06-16 18:49:42 +0000

    modify model

--- train.py
+++ train.py
@@ -28,13 +28,20 @@ X_train = np.array(images)
 y_train = np.array(steerings)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D, Dropout
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((75,25), (0,0))))
+model.add(Lambda(lambda x: x/255.0-0.5, input_shape=(60,320,3)))
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.25))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
 model.save('model.h5')
                
